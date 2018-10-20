dfac48acede713dd637d2fd1f2b77a0ed145874b
mas16 <mattstetz@gmail.com>
2018-10-20 17:02:48 -0400

    style cleanup

--- heros_sqrt_v2.py
+++ heros_sqrt_v2.py
@@ -41,35 +41,35 @@ PRECISION = 0.001
 
 #####################################
 
-#function to perform hero's method
-def hero(x,guess,iterations,precision):
+#Function to perform hero's method
+def hero(x, guess, iterations, precision):
      i = 0
      guess_log = [guess]
      counter_log = [i]
-     print ('Inital guess: '+str(guess)+'\n')
-     #check convergence with while loop to avoid memory limits
+     print ('Inital guess: ' + str(guess) + '\n')
+     #Check convergence with while loop to avoid memory limits
      #for large number of iterations
      while i <= iterations:
-          #this conditional style is supported in Python
+          #This conditional style is supported in Python
           if x-precision < guess**2 < x+precision:
-               print ('\nSolution Found = '+str(guess))
-               print ('Solution Squared = '+str(guess**2))
+               print ('\nSolution Found = ' + str(guess))
+               print ('Solution Squared = ' + str(guess**2))
                break
-          #new guess
+          #New guess
           else:
                guess = (guess+(x/guess))/2.0
                guess_log.append(guess)
                counter_log.append(i)
-               print ('New guess '+str(i+1)+': '+str(guess))
+               print ('New guess ' + str(i+1) + ': ' + str(guess))
                i += 1
      return guess_log,counter_log
 
-#visualize results
-def plot(guess_log,counter_log,x):
+#Visualize results
+def plot(guess_log, counter_log, x):
      correct_sqrt = np.sqrt(x)
      x_axis = np.linspace(-1, 1.1*len(guess_log), len(guess_log))
      y_axis = np.ones(len(guess_log))*correct_sqrt
-     plt.plot(counter_log, guess_log,'ok')
+     plt.plot(counter_log, guess_log, 'ok')
      plt.plot(x_axis, y_axis, '--r', linewidth=2.0, label='Correct Answer')
      plt.axis([-1, max(x_axis), -0.1*max(guess_log), 1.1*max(guess_log)])
      plt.xlabel('Iterations')
@@ -79,5 +79,5 @@ def plot(guess_log,counter_log,x):
      return 0
 
 if __name__ == '__main__':
-     GUESS_LOG,COUNTER_LOG = hero(X,GUESS,ITERATIONS,PRECISION)
-     plot(GUESS_LOG,COUNTER_LOG,X)
+     GUESS_LOG, COUNTER_LOG = hero(X, GUESS, ITERATIONS, PRECISION)
+     plot(GUESS_LOG, COUNTER_LOG, X)
